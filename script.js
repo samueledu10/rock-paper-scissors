81c0521afcdc742261bb2ade483db0e8c2123b66
Samuel Alvarenga <69541465+samueledu10@users.noreply.github.com>
2023-07-06 19:58:54 -0400

    Add function that plays first round

--- script.js
+++ script.js
@@ -15,3 +15,23 @@ function getComputerChoice() {
 }
 
 
+function playRound(playerSelection, computerSelection) {
+    let lowerCasePlayerSelection = playerSelection.toLowerCase();
+
+    if (lowerCasePlayerSelection === computerSelection.toLowerCase()) {
+        return "It's a tie!";
+    } 
+
+    if (lowerCasePlayerSelection === "rock" && computerSelection === "Scissors") {
+        return "You win! Rock beats Scissors";
+    } else if (lowerCasePlayerSelection === "paper" && computerSelection === "Rock") {
+        return "You win! Paper beats Rock";
+    } else if (lowerCasePlayerSelection === "scissors" && computerSelection === "Paper") {
+        return "You win! Scissors beat Paper";
+    } else {
+        return "You lose! " + computerSelection + " beats " + lowerCasePlayerSelection.replace(lowerCasePlayerSelection.charAt(0), lowerCasePlayerSelection.charAt(0).toUpperCase());
+    }
+}
+let cp = getComputerChoice();
+console.log(cp);
+console.log(playRound("Paper", cp));
